5a17a4aa7236b1e3e59d8ddba50bbe93f706928a
Nikita <nerv.21vp1@gmail.com>
2025-10-07 20:38:27 +0300

    Added browserName parameter

--- src/test/java/dev/nervvvana/qa/hooks/Hooks.java
+++ src/test/java/dev/nervvvana/qa/hooks/Hooks.java
@@ -7,23 +7,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
     public static WebDriver driver;
 
+    private static Properties properties = new Properties();
     @Before
     public void before() {
-        System.setProperty("webdriver.geckodriver.driver", "src/test/resources/geckodriver.exe");
+        loadApplicationProperties();
+        //System.setProperty("webdriver.geckodriver.driver", "src/test/resources/geckodriver.exe");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         Map<String, Object> selenoidOptions = new HashMap<>();
-        capabilities.setCapability("browserName", "firefox");
+        capabilities.setCapability("browserName",
+                System.getProperty("browser.name", properties.getProperty("browser.name")));
         capabilities.setCapability("browserVersion", "109.0");
         selenoidOptions.put("enableVNC", true);
         selenoidOptions.put("enableVideo", false);
@@ -31,7 +38,7 @@ public class Hooks {
 
         try {
             driver = new RemoteWebDriver(
-                    URI.create("http://jenkins.applineselenoid.fvds.ru:4444/wd/hub/").toURL(),
+                    URI.create(properties.getProperty("selenoid.url")).toURL(),
                     capabilities
             );
         } catch (MalformedURLException e) {
@@ -50,4 +57,15 @@ public class Hooks {
             driver.quit();
         }
     }
+
+    private void loadApplicationProperties() {
+        try {
+            properties.load(new FileInputStream(
+                    new File("src/main/resources/" +
+                            System.getProperty("propFile", "application") + ".properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
