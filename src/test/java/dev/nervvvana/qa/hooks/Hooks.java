ee9321e1434a86ab24429bdae4a8acae9e75a79d
Nikita <nerv.21vp1@gmail.com>
2025-10-07 19:55:14 +0300

    Added selenoid connection

--- src/test/java/dev/nervvvana/qa/hooks/Hooks.java
+++ src/test/java/dev/nervvvana/qa/hooks/Hooks.java
@@ -4,7 +4,13 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
@@ -15,9 +21,25 @@ public class Hooks {
     public void before() {
         System.setProperty("webdriver.geckodriver.driver", "src/test/resources/geckodriver.exe");
 
-        driver = new FirefoxDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        capabilities.setCapability("browserName", "firefox");
+        capabilities.setCapability("browserVersion", "109.0");
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", false);
+        capabilities.setCapability("selenoid:options", selenoidOptions);
+
+        try {
+            driver = new RemoteWebDriver(
+                    URI.create("http://jenkins.applineselenoid.fvds.ru:4444/wd/hub/").toURL(),
+                    capabilities
+            );
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
